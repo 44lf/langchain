f8e2caa3862fd5cd0589e543ca8ed49df0a91706
豆豆又包包 <1619728742@qq.com>
2026-01-29 20:01:19 +0800

    23

--- app/utils/llm.py
+++ app/utils/llm.py
@@ -1,29 +1,22 @@
-from dotenv import load_dotenv
-# from langchain.agents import create_agent
-from langchain_deepseek.chat_models import ChatDeepseek 
 import os
+
+from dotenv import load_dotenv
+from langchain_deepseek.chat_models import ChatDeepseek
 
 load_dotenv()
 
+
 class LLMUtils:
-    # @staticmethod
-    # def create_agent(llm):
-    #     agent = create_agent(
-    #         model=llm,
-    #         system="你是一个乐于助人的助手")
-    #     return agent
-
-
-    def init_llm():
-        llm = ChatDeepseek(
+    @staticmethod
+    def init_llm() -> ChatDeepseek:
+        return ChatDeepseek(
             model="deepseek-chat",
             temperature=0,
             max_retries=3,
-            api_key=os.getenv("DEEPSEEK_API_KEY")
+            api_key=os.getenv("DEEPSEEK_API_KEY"),
         )
-        return llm
-    
 
 
-llm = init_llm()
-ll.invoke('Hello, how can I assist you today?')
+if __name__ == "__main__":
+    llm = LLMUtils.init_llm()
+    llm.invoke("Hello, how can I assist you today?")
